ff2b225698e21887da7d6334d0f0eb927cb91931
Youri <89806453+YouriLieverdink@users.noreply.github.com>
2022-10-12 21:21:43 +0200

    fix: add support for 2.15 json generator again (#24)

---
+++ services/server/lib/src/domain/common/constants.dart
@@ -0,0 +1,9 @@
+const unions = [
+  'Nonet', 'Singlet', 'Doublet', 'Triplet', 'Quartet', 'Quintet', 'Sextet',
+  'Septet', 'Octet', 'Nonet', //
+];
+
+const factories = [
+  'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth',
+  'ninth' //
+];
