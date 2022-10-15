b54f77f3de243c04410640c2e0f9e40443922db8
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-10-15 08:52:40 +0200

    feat: add the core of the architect package

---
+++ packages/architect/lib/src/domain/utilities/identifier.dart
@@ -0,0 +1,26 @@
+import 'package:recase/recase.dart';
+
+const keywords = [
+  'abstract', 'else', 'import', 'super', 'as', 'enum', 'in', 'switch',
+  'assert', 'export', 'interface', 'sync', 'async', 'extends', 'is', 'this',
+  'await', 'extension', 'library', 'throw', 'break', 'external', 'mixin',
+  'true', 'case', 'factory', 'new', 'try', 'class', 'final', 'catch',
+  'false', 'null', 'typedef', 'on', 'var', 'const', 'finally', 'operator',
+  'void', 'continue', 'for', 'part', 'while', 'covariant', 'Function',
+  'rethrow', 'with', 'default', 'get', 'return', 'yield', 'deferred', 'hide',
+  'set', 'do', 'if', 'show', 'dynamic', 'implements', 'static', //
+];
+
+/// Adds an underscore (_) to the value when it is a reserver keyword in Dart.
+String escapeIfKeyword(
+  String value,
+) {
+  return keywords.contains(value.trim()) ? '${value}_' : value;
+}
+
+/// Returns an camel case identifier.
+String getName(
+  String value,
+) {
+  return escapeIfKeyword(value.camelCase);
+}
