04ca26493957cdb5e26ede5eef3de9cfff9f8901
Youri <89806453+YouriLieverdink@users.noreply.github.com>
2022-10-16 11:25:30 +0200

    feat: implement a message send on invocation error (#34)

--- packages/architect_server/lib/src/operations/invocation/add_invocation.dart
+++ packages/architect_server/lib/src/operations/invocation/add_invocation.dart
@@ -1,16 +1,22 @@
 import 'package:architect/architect.dart';
 
 import '../../generated/io_apibuilder_generator_v0_json.dart' as i1;
+import '../../generated/nl_pipbo_slack_v0_json.dart' as i2;
 
 class CreateAddInvocation {
   //
   const CreateAddInvocation(
     this.showGenerator,
+    this.addMessage,
   );
 
   final Future<Generator> Function(
     String key,
   ) showGenerator;
+
+  final Future<void> Function(
+    i2.Message message,
+  ) addMessage;
 
   Future<i1.Invocation> call(
     String key,
@@ -24,6 +30,13 @@ class CreateAddInvocation {
       return i1.Invocation.fromJson(output);
     } //
     catch (e) {
+      final message = i2.Message(
+        text: e.toString(),
+        username: 'architect',
+      );
+
+      await addMessage(message);
+
       throw i1.Error(
         code: 'invocation-error',
         message: e.toString(),
